40564f4554c804d57817170ab070d976e73bc28d
doctypehtml <195564139+docccccccccccc@users.noreply.github.com>
2026-08-13 01:54:05 +0800

    添加基本的移动端适配布局

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
+  <div class="celquoteoptions-wrapper">
     <el-card>
       <template #header> <el-text size="large">天神语录生成器</el-text> </template>
-      1234 Test 测试
+      <main class="celquoteoptions-main">
+        <el-text>
+          1234 Test 测试
+          loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
+        </el-text>
+      </main>
       <template #footer> <el-text size="large">本应用使用了 MiSans 字体</el-text> </template>
     </el-card>
-  </main>
+  </div>
 </template>
 
-<style scoped>
-.el-card {
-  width: 100%;
-  max-width: 1440px;
+<style scoped lang="scss">
+.celquoteoptions-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100svh;
+  .el-card {
+    max-width: 1080px;
+  }
+
+  .celquoteoptions-main {
+    overflow: hidden;
+  }
+}
+
+@media screen and (max-width: 720px) {
+  .el-card {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
