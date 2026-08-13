8496310cab9630cefe244534a62bd811bc712829
doctypehtml <195564139+docccccccccccc@users.noreply.github.com>
2026-08-13 18:03:33 +0800

    添加更多用于背景的符号预设

--- src/consts/bgPresets.ts
+++ src/consts/bgPresets.ts
@@ -1,10 +1,11 @@
 export const SYMBOLS_FOR_BG: { value: string }[] = [
+  // 天神
   { value: 'Ϟ' }, // 特蕾莎
   { value: 'ϙ' }, // 鹿颈长
   { value: '⌬' }, // 薇
   { value: 'ᛝ' }, // 莱特拉
   { value: '♅' }, // 佩勒
-  { value: 'α' }, // 阿尔法（AD: Endgame）
+  // 符文
   { value: 'Ω' }, // 力量符文
   { value: '∞' }, // 无限符文
   { value: 'Ξ' }, // 复制符文
@@ -12,6 +13,21 @@ export const SYMBOLS_FOR_BG: { value: string }[] = [
   { value: 'ψ' }, // 膨胀符文
   { value: '✝' }, // 诅咒符文
   { value: '♥' }, // 同伴符文
+  // 其他符文炼金资源
+  { value: 'α' }, // 基数
+  { value: 'τ' }, // 永恒
+  { value: 'ρ' }, // 维度
+  { value: 'λ' }, // 暴涨
+  { value: 'ω' }, // 异变
+  { value: 'π' }, // 协同
+  { value: 'ξ' }, // 退相干
+  { value: 'μ' }, // 动量
+  { value: 'Σ' }, // 诸界奥义
+  { value: 'Φ' }, // 神秘力场
+  { value: 'Γ' }, // 增幅秘术
+  { value: 'Θ' }, // 无尽奥秘
+  { value: 'Π' }, // 无限虚空
+  { value: 'Λ' }, // 未知预言
 ]
 
 export const FA_ICON_IDS_FOR_BG: { value: string }[] = [
